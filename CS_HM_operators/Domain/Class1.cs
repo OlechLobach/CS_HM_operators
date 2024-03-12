951e55239805c52e5b006ce976b4c87ce944a961
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-13 00:18:45 +0200

    initiall commit

--- CS_HM_operators/Domain/Class1.cs
+++ CS_HM_operators/Domain/Class1.cs
@@ -1,1 +1,76 @@
-﻿
+﻿using System;
+
+namespace Domain
+{
+    public class Shop
+    {
+        private string name;
+        private string address;
+        private string description;
+        private string phoneNumber;
+        private string email;
+        private double area; // Площа магазину
+
+        public Shop(string name, string address, string description, string phoneNumber, string email, double area)
+        {
+            this.name = name;
+            this.address = address;
+            this.description = description;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.area = area;
+        }
+
+        public void DisplayShopInformation()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Address: {address}");
+            Console.WriteLine($"Description: {description}");
+            Console.WriteLine($"Phone Number: {phoneNumber}");
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine($"Area: {area}");
+        }
+
+        public static Shop operator +(Shop shop, double sizeIncrease)
+        {
+            shop.area += sizeIncrease;
+            return shop;
+        }
+
+        public static Shop operator -(Shop shop, double sizeDecrease)
+        {
+            shop.area -= sizeDecrease;
+            return shop;
+        }
+
+        public static bool operator ==(Shop shop1, Shop shop2)
+        {
+            return shop1.area == shop2.area;
+        }
+
+        public static bool operator !=(Shop shop1, Shop shop2)
+        {
+            return shop1.area != shop2.area;
+        }
+
+        public static bool operator <(Shop shop1, Shop shop2)
+        {
+            return shop1.area < shop2.area;
+        }
+
+        public static bool operator >(Shop shop1, Shop shop2)
+        {
+            return shop1.area > shop2.area;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Shop shop = (Shop)obj;
+            return area.Equals(shop.area);
+        }
+    }
+}
--- CS_HM_operators/Program.cs
+++ CS_HM_operators/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+using Domain;
+
+namespace MainProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Shop shop1 = new Shop("Shop1", "Address1", "Description1", "123456789", "shop1@example.com", 100.0);
+            Shop shop2 = new Shop("Shop2", "Address2", "Description2", "987654321", "shop2@example.com", 150.0);
+
+
+            Console.WriteLine("Shop1 Information:");
+            shop1.DisplayShopInformation();
+
+            Console.WriteLine("\nShop2 Information:");
+            shop2.DisplayShopInformation();
+
+            shop1 += 50.0;
+            Console.WriteLine("\nShop1 Area Increased by 50.0:");
+            shop1.DisplayShopInformation();
+
+            if (shop1 == shop2)
+            {
+                Console.WriteLine("\nShop1 and Shop2 have the same area.");
+            }
+            else
+            {
+                Console.WriteLine("\nShop1 and Shop2 have different areas.");
+            }
+
+            if (shop1 > shop2)
+            {
+                Console.WriteLine("\nShop1 has a larger area than Shop2.");
+            }
+            else
+            {
+                Console.WriteLine("\nShop1 does not have a larger area than Shop2.");
+            }
+        }
+    }
+}
