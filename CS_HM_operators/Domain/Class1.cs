df285392701ec94b214d0c3945325dcfcf7f1232
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-13 00:07:47 +0200

    initiall commit

--- CS_HM_operators/Domain/Class1.cs
+++ CS_HM_operators/Domain/Class1.cs
@@ -1,1 +1,89 @@
-﻿
+﻿using System;
+
+namespace Domain
+{
+    public class Journal
+    {
+        private string name;
+        private int yearFounded;
+        private string description;
+        private string contactPhone;
+        private string email;
+        private int numberOfEmployees;
+
+        public Journal(string name, int yearFounded, string description, string contactPhone, string email, int numberOfEmployees)
+        {
+            this.name = name;
+            this.yearFounded = yearFounded;
+            this.description = description;
+            this.contactPhone = contactPhone;
+            this.email = email;
+            this.numberOfEmployees = numberOfEmployees;
+        }
+
+        public int NumberOfEmployees
+        {
+            get { return numberOfEmployees; }
+            set { numberOfEmployees = value; }
+        }
+
+        public void DisplayJournalInformation()
+        {
+            Console.WriteLine("Journal Information:");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Year Founded: {yearFounded}");
+            Console.WriteLine($"Description: {description}");
+            Console.WriteLine($"Contact Phone: {contactPhone}");
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine($"Number of Employees: {numberOfEmployees}");
+        }
+
+        public static Journal operator +(Journal journal, int employeesToAdd)
+        {
+            journal.numberOfEmployees += employeesToAdd;
+            return journal;
+        }
+
+        public static Journal operator -(Journal journal, int employeesToRemove)
+        {
+            journal.numberOfEmployees -= employeesToRemove;
+            return journal;
+        }
+
+        public static bool operator ==(Journal journal1, Journal journal2)
+        {
+            return journal1.numberOfEmployees == journal2.numberOfEmployees;
+        }
+
+        public static bool operator !=(Journal journal1, Journal journal2)
+        {
+            return journal1.numberOfEmployees != journal2.numberOfEmployees;
+        }
+
+        public static bool operator <(Journal journal1, Journal journal2)
+        {
+            return journal1.numberOfEmployees < journal2.numberOfEmployees;
+        }
+
+        public static bool operator >(Journal journal1, Journal journal2)
+        {
+            return journal1.numberOfEmployees > journal2.numberOfEmployees;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Journal other = (Journal)obj;
+            return this.numberOfEmployees == other.numberOfEmployees;
+        }
+
+        public override int GetHashCode()
+        {
+            return numberOfEmployees.GetHashCode();
+        }
+    }
+}
--- CS_HM_operators/Program.cs
+++ CS_HM_operators/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using Domain;
+
+namespace MainProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Journal journal1 = new Journal("Journal1", 2000, "Description1", "123456789", "journal1@example.com", 50);
+            Journal journal2 = new Journal("Journal2", 2010, "Description2", "987654321", "journal2@example.com", 75);
+
+            journal1.DisplayJournalInformation();
+            journal2.DisplayJournalInformation();
+
+            journal1 += 10; 
+            journal2 -= 5; 
+
+            Console.WriteLine("\nAfter adding/removing employees:");
+            journal1.DisplayJournalInformation();
+            journal2.DisplayJournalInformation();
+
+            Console.WriteLine($"\nAre the number of employees in journal1 and journal2 equal? {journal1 == journal2}");
+
+            Console.WriteLine($"Are the number of employees in journal1 and journal2 not equal? {journal1 != journal2}");
+
+            Console.WriteLine($"Is the number of employees in journal1 less than in journal2? {journal1 < journal2}");
+            Console.WriteLine($"Is the number of employees in journal1 greater than in journal2? {journal1 > journal2}");
+        }
+    }
+}
