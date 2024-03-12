e678924a7eb6d96330aa4988254d6d863d101408
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-13 00:28:55 +0200

    initiall commit

--- CS_HM_operators/Domain/Class1.cs
+++ CS_HM_operators/Domain/Class1.cs
@@ -1,1 +1,87 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookList
+{
+    public class BookList
+    {
+        private List<string> books;
+
+        public BookList()
+        {
+            books = new List<string>();
+        }
+
+        public void AddBook(string book)
+        {
+            books.Add(book);
+        }
+
+        public void RemoveBook(string book)
+        {
+            books.Remove(book);
+        }
+
+        public bool ContainsBook(string book)
+        {
+            return books.Contains(book);
+        }
+
+        public void DisplayBooks()
+        {
+            Console.WriteLine("Books in the list:");
+            foreach (var b in books)
+            {
+                Console.WriteLine(b);
+            }
+        }
+
+        public int Count
+        {
+            get { return books.Count; }
+        }
+
+        public string this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+
+        public static BookList operator +(BookList list, string book)
+        {
+            list.AddBook(book);
+            return list;
+        }
+
+        public static BookList operator -(BookList list, string book)
+        {
+            list.RemoveBook(book);
+            return list;
+        }
+
+        public static bool operator ==(BookList list, string book)
+        {
+            return list.ContainsBook(book);
+        }
+
+        public static bool operator !=(BookList list, string book)
+        {
+            return !list.ContainsBook(book);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            BookList other = (BookList)obj;
+            return books.SequenceEqual(other.books);
+        }
+
+        public override int GetHashCode()
+        {
+            return books.GetHashCode();
+        }
+    }
+}
--- CS_HM_operators/Program.cs
+++ CS_HM_operators/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using BookList;
+using System;
+
+namespace BookListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookList.BookList list = new BookList.BookList();
+
+            list.AddBook("Book1");
+            list.AddBook("Book2");
+            list.AddBook("Book3");
+
+            list.DisplayBooks();
+
+            Console.WriteLine("Removing Book2");
+            list.RemoveBook("Book2");
+
+            list.DisplayBooks();
+
+            Console.WriteLine("Is 'Book1' in the list? " + (list == "Book1"));
+
+            Console.WriteLine("Number of books: " + list.Count);
+
+            Console.WriteLine("Book at index 0: " + list[0]);
+
+            list[0] = "NewBook1";
+
+            Console.WriteLine("Updated book at index 0: " + list[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
