83f60d2f2ff80c99fca0cbbf97778fcaecd7b8c1
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-13 00:05:47 +0200

    initiall commit

--- CS_HM_operators/Domain/Class1.cs
+++ CS_HM_operators/Domain/Class1.cs
@@ -1,1 +1,75 @@
-﻿
+﻿using System;
+
+namespace Domain
+{
+    public class CreditCard
+    {
+        private string cardNumber;
+        private int cvc;
+        private DateTime expiryDate;
+        private decimal balance;
+
+        public string CardNumber { get { return cardNumber; } }
+        public int Cvc { get { return cvc; } }
+        public DateTime ExpiryDate { get { return expiryDate; } }
+        public decimal Balance { get { return balance; } }
+
+        public CreditCard(string cardNumber, int cvc, DateTime expiryDate, decimal balance)
+        {
+            this.cardNumber = cardNumber;
+            this.cvc = cvc;
+            this.expiryDate = expiryDate;
+            this.balance = balance;
+        }
+
+        public static CreditCard operator +(CreditCard card, decimal amount)
+        {
+            card.balance += amount;
+            return card;
+        }
+
+        public static CreditCard operator -(CreditCard card, decimal amount)
+        {
+            card.balance -= amount;
+            return card;
+        }
+
+        public static bool operator ==(CreditCard card1, CreditCard card2)
+        {
+            return card1.Cvc == card2.Cvc;
+        }
+
+        public static bool operator !=(CreditCard card1, CreditCard card2)
+        {
+            return card1.Cvc != card2.Cvc;
+        }
+
+        public static bool operator <(CreditCard card1, CreditCard card2)
+        {
+            return card1.Balance < card2.Balance;
+        }
+
+        public static bool operator >(CreditCard card1, CreditCard card2)
+        {
+            return card1.Balance > card2.Balance;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            CreditCard card = (CreditCard)obj;
+            return cvc == card.cvc;
+        }
+
+        public void DisplayCardInformation()
+        {
+            Console.WriteLine($"Card Number: {cardNumber}");
+            Console.WriteLine($"CVC: {cvc}");
+            Console.WriteLine($"Expiry Date: {expiryDate}");
+            Console.WriteLine($"Balance: {balance}");
+        }
+    }
+}
--- CS_HM_operators/Program.cs
+++ CS_HM_operators/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System;
+using Domain;
+
+namespace MainProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            CreditCard card1 = new CreditCard("1234567890123456", 123, new DateTime(2024, 12, 31), 1000.0m);
+            CreditCard card2 = new CreditCard("9876543210987654", 456, new DateTime(2025, 12, 31), 2000.0m);
+
+            Console.WriteLine("Initial Card Information:");
+            Console.WriteLine("Card 1:");
+            card1.DisplayCardInformation();
+            Console.WriteLine("\nCard 2:");
+            card2.DisplayCardInformation();
+
+            Console.WriteLine("\nAdding 500 to Card 1...");
+            card1 += 500.0m;
+            Console.WriteLine("New Balance for Card 1: " + card1.Balance);
+
+            Console.WriteLine("\nSubtracting 300 from Card 2...");
+            card2 -= 300.0m;
+            Console.WriteLine("New Balance for Card 2: " + card2.Balance);
+
+            Console.WriteLine("\nChecking if Card 1 and Card 2 have the same CVC...");
+            if (card1 == card2)
+            {
+                Console.WriteLine("Card 1 and Card 2 have the same CVC.");
+            }
+            else
+            {
+                Console.WriteLine("Card 1 and Card 2 do not have the same CVC.");
+            }
+
+            Console.WriteLine("\nChecking if Card 1 has more balance than Card 2...");
+            if (card1 > card2)
+            {
+                Console.WriteLine("Card 1 has more balance than Card 2.");
+            }
+            else
+            {
+                Console.WriteLine("Card 1 does not have more balance than Card 2.");
+            }
+        }
+    }
+}
